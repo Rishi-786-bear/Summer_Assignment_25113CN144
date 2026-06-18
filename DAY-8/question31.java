0a598c257090eeff5749f1b58c9b62c0aa7e67ce
Rishi-786-bear <rishi982007@gmail.com>
2026-06-18 19:10:30 +0530

    Add files via upload

---
+++ DAY-8/question31.java
@@ -0,0 +1,21 @@
+// Write a program to Print character triangle.
+import java.util.Scanner;
+
+public class question31 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+
+        for (int i = 1; i <= rows; i++) {
+            for (char ch = 'A'; ch < 'A' + i; ch++) {
+                System.out.print(ch + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+    
+}
