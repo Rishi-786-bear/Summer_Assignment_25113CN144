a528132b1864f0eef9f624c78a21017642161176
Rishi-786-bear <rishi982007@gmail.com>
2026-06-18 19:12:10 +0530

    Add files via upload

---
+++ DAY-9/question35.java
@@ -0,0 +1,24 @@
+//Write a program to Print repeated character pattern.
+
+import java.util.Scanner;
+
+public class question35 {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+
+        for (int i = 1; i <= rows; i++) {
+            char ch = (char) ('A' + i - 1);
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch + " ");
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
