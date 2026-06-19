260893a9cc84f9a49229b425af976bbafd9b4bf6
Rishi-786-bear <rishi982007@gmail.com>
2026-06-19 12:43:13 +0530

    Add files via upload

---
+++ DAY-13/question49.java
@@ -0,0 +1,33 @@
+// Write a program to input and display array
+
+import java.util.Scanner;
+
+public class question49 {
+      public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        // Input array elements
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Display array elements
+        System.out.println("Array elements are:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+
+
+
+
+
+}
---
+++ DAY-13/question50.java
@@ -0,0 +1,28 @@
+//Write a program to Find sum and average of array
+import java.util.Scanner;
+
+public class question50 {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        int sum = 0;
+
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+
+        double average = (double) sum / n;
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Average = " + average);
+
+        sc.close();
+    }
+    
+}
